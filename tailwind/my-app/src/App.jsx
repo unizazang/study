c156d0a1c7faf6588e039ffb53644fbe251da9b3
unijajang <zifnffk32123@gmail.com>
2025-02-11 21:07:35 +0900

    next.js 프로젝트 생성

--- tailwind/my-app/src/App.jsx
+++ tailwind/my-app/src/App.jsx
@@ -5,17 +5,15 @@ import './App.css'
 
 function App() {
   
-let [cnt, setCnt ] = useState();
+let [cnt, setCnt ] = useState(0);
   return (
-<div>
-  <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-    <img className="size-12 shrink-0" src="/assets/react.svg" alt="ChitChat Logo" />
     <div>
-      <div className="text-xl font-medium text-black dark:text-white">ChitChat</div>
-      <p className="text-gray-500 dark:text-gray-400">You have a new message!</p>
+      <h1 className='text-blue font- '> 안녕하세요? </h1>
+      <h2> {cnt }</h2>
+      <button className=' bg-blue w-96 h-10 text-red-950 rounded-full py-2 ring-8 ring-pink'  onClick={()=>{
+        setCnt(cnt+1)
+      }}> COUNT </button>
     </div>
-  </div>
-</div>
   )
 }
 
